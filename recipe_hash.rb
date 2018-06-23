4b2248ee51264867fd42262b0356074585a7a241
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-23 21:12:33 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -12,6 +12,18 @@
 #  - olive oil - 1/2 cup
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
+perfect_10_ingredients = {
+  :almond_flour => "2 cups",
+  :gluten_free_whole_oats => "1 cup",
+  :kosher_salt => "1/2 tea spoon",
+  :baking_powder => "1/2 tea spoon",
+  :baking_soda => "1/4 tea spoon",
+  :xanthan_Gum => "1/2 tea spoon",
+  :slivered_almonds => "1/4 cup",
+  :mini_dark_chocolate_chips => "3/4 cup",
+  :olive_oil => "1/2 cup",
+  :agave => "1/4 cup"
+}
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
